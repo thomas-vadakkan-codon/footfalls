9de90a51b77e7fa12538b6cc85017fffb60f0f63
rohanmodepalle <rohan.modepalle@students.iiit.ac.in>
2022-06-14 13:40:23 +0530

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
 
-os.system("python Run.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel")# --input videos/example_01.mp4")
+os.system("python Run.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4")
 
 
