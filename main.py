5feb262996116efd933cd9a2d97ee91ca0403845
rohanmodepalle <rohan.modepalle@students.iiit.ac.in>
2022-06-14 11:10:58 +0530

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
 
 #run a shell command using os
-os.system("python Run.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel")# --input videos/example_01.mp4")
+os.system("python Run.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4")
